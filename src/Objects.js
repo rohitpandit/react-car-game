e09e96eb927e201ddc2dec2f8e7a5eb9c1fa5bfa
Rohit Pandit <rohit0311@outlook.com>
2020-12-23 15:22:24 +0530

    truck

--- src/Objects.js
+++ src/Objects.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import Car from './Car.js';
+import Truck from './Truck.js';
 
 function Objects(){
     return (
         <div>
+            <Truck className="truck1"/>
+            <Truck className="truck1"/>
+            <Truck className="truck1"/>
+            
             <Car />
         </div>
     );
---
+++ src/Truck.js
@@ -0,0 +1,12 @@
+import React from "react";
+import './Truck.css';
+
+function Truck(){
+    return(
+        <div className="truck">
+            
+        </div>
+    )
+}
+
+export default Truck;
