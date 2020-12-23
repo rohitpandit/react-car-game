b540bae6e12921571796db59ac906cddce38b1e0
Rohit Pandit <rohit0311@outlook.com>
2020-12-23 14:28:22 +0530

    car moving

--- src/Car.js
+++ src/Car.js
@@ -1,12 +1,37 @@
 import React from 'react';
 import './Car.css';
 
-function Car(){
-    return(
-        <div className="car">
-            <img src="./car.png" />
-        </div>
+
+class Car extends React.Component {
+    constructor(props){
+        super(props);
+        this.state={
+            posX:0
+        }
+    }
+
+    componentDidMount(){
+      document.addEventListener('keydown', this.handleKeyDown.bind(this));
+    }
+
+    handleKeyDown=(e)=>{
+      if(e.keyCode === 37){
+        this.setState((state, props)=>({
+          posX : Math.max(state.posX-1, 0)
+        }));
+      }else if(e.keyCode === 39){
+        this.setState((state, props)=>({
+          posX : Math.min(state.posX+1, 25)
+        }))
+      }
+    }
+
+  render() {
+    return (
+      <div  className="car" style={{left:this.state.posX +'rem'}}>
+      </div>
     );
+  }
 }
 
 export default Car;
